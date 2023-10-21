a024e6a9eb22d5225a2abb65167362e18aef8cc3
Lyudmyla26 <124089916+Lyudmyla26@users.noreply.github.com>
2023-10-21 16:14:00 +0300

    f

---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <>
+      <div>
+        <h2>Log in and I help you save and filter your favorite contacts</h2>
+        <h3>Your Phonebook</h3>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
